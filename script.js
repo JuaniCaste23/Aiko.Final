4046f07de83a3a16e67d1166da781e3db0c75658
JuaniCaste23 <juanicaste23@gmail.com>
2024-08-13 14:13:15 -0300

    subiendo cambios de estructura

--- script.js
+++ script.js
@@ -1,26 +1,71 @@
-//variables
+// /*Variables*/
+// const iconMenu = document.querySelector('.icon-menu');
+// const mobileMenu = document.querySelector('.mobile-menu');
 
-//funciones
+// /*Funciones*/
+// const desplegarMenu = () => {
 
+//     mobileMenu.style.display = 'block';
+//     iconMenu.style.opacity = '50%';
+//     iconMenu.style.transform = 'rotate(90deg)';
+    
+// };
 
+// const replegarMenu = () => {
+
+//     mobileMenu.style.display = 'none';
+//     iconMenu.style.opacity = '100%';
+//     iconMenu.style.transform = 'rotate(0deg)';
+
+// };
+
+// const displayIconMenu = (e) => {
+
+//     e.preventDefault();
+
+//     if ( mobileMenu.style.display === 'block' )
+//     {
+//         replegarMenu();
+//     }
+//     else
+//     {
+//         desplegarMenu();
+//     }
+    
+// };
+
+// const defaultPage = (e) => {
+
+//     e.preventDefault();
+
+//     if ( window.innerWidth >= 1024 )
+//     {
+//         replegarMenu();
+//     }
+
+// };
+
+// /*Eventos*/
+// iconMenu.addEventListener('click', displayIconMenu);
+
+// window.addEventListener("resize", defaultPage);
 
 /*Funcion externa*/
 //funcion que permite un scroll suavizado entre secciones
-const links = document.querySelectorAll('a');
+const links = document.querySelectorAll('a[href^="#"]');
 
-links.forEach((link) => {
+    links.forEach((link) => {
 
-    link.addEventListener('click', (e) => {
-        
-        const target = e.currentTarget.getAttribute('href');
+        link.addEventListener('click', (e) => {
+            
+            const target = e.currentTarget.getAttribute('href');
 
-        e.preventDefault();
-        replegarMenu();
-        document.querySelector(target).scrollIntoView(
-        {
-            behavior: 'smooth'
+            e.preventDefault();
+            document.querySelector(target).scrollIntoView(
+            {
+                behavior: 'smooth'
+            });
+
         });
 
-    });
-
-}); 
+    }); 
